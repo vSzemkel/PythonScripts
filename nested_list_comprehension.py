38176301ac08d37707f463905d576b8de8214ceb
Marcin Buchwald <marcin.buchwald@agora.pl>
2020-08-06 08:49:33 +0200

    [ADD] Nested comprehension list2

--- nested_list_comprehension.py
+++ nested_list_comprehension.py
@@ -6,3 +6,7 @@ It demostrates how to interpret a nested list comprehension
 a = "30 PLN,12 USD,9 EUR,307 GBP"
 q = [x for pair in a.split(',') for i, x in enumerate(pair.split(' ')) if i == 0]
 print(q)
+
+'''
+Outputs: ['30', '12', '9', '307']
+'''
